16f278ff5f6e54553fce2a8c71d9e601b03df0d6
Gunnar Mann <dev@sternmotor.net>
2025-02-17 14:54:14 +0100

    Moved buferloine config into own file

---
+++ lua/plugins/configs/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  require('bufferline').setup {
+    options = {
+      mode = 'buffers',
+      separator_style = 'slant',
+      always_show_bufferline = true,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      color_icons = true,
+    },
+  }
+}
